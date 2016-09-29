7c19078c5fce6a9ccb412820eb72859d5d1e82a3
ankit <ankit@happy2refer.com>
2016-09-29 20:35:51 +0530

    initial commit

---
+++ utils/src/main/java/com/aps/utils/UtilityConstants.java
@@ -0,0 +1,10 @@
+package com.aps.utils;
+
+public class UtilityConstants {
+
+    public static final String BLANK = "";
+    public static final Integer ZERO = 0;
+    public static final String STRING_ZERO = "0";
+    public static final Boolean VALUE_FALSE = Boolean.FALSE;
+
+}
